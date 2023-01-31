7395d3600b4e2d67eaf6b206f3b562ac2f0826b7
isademir <isademir.dev@gmail.com>
2023-02-01 02:12:26 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        private bool isCarExist(Car car)
+        {
+            var carCheck = _carDal.GetById(car.Id);
+            if (carCheck != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Add(Car car)
+        {
+            if (!isCarExist(car))
+            {
+                Console.WriteLine("Araba başarıyla eklendi.");
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Girilen ID değerinde araç var!");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            if (!isCarExist(car))
+            {
+                _carDal.Delete(car);
+            }
+            else
+            {
+                Console.WriteLine("Silinemedi! Belirtilen değere sahip araç yok!");
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public Car GetById(int id)
+        {
+            return _carDal.GetById(id);
+        }
+
+        public void Update(Car car)
+        {
+            if (isCarExist(car))
+            {
+                _carDal.Update(car);
+            }
+            else
+            {
+                Console.WriteLine("Güncellenecek araç yok!");
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarDal inMemorCarDal = new InMemoryCarDal();
+            ICarService carService = new CarManager(inMemorCarDal);
+
+            var araclar = carService.GetAll();
+            foreach (var c in araclar)
+            {
+                Console.WriteLine($"         Id : {c.Id}");
+                Console.WriteLine($"    BrandId : {c.BrandId}");
+                Console.WriteLine($"    ColorId : {c.ColorId}");
+                Console.WriteLine($"      Model : {c.ModelYear}");
+                Console.WriteLine($"Daily Price : {c.DailyPrice} TL");
+                Console.WriteLine($"Description : {c.Description}");
+                Console.WriteLine("------------------------------");
+            }
+        }
+    }
+}
